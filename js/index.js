e766c8e53cc2f08085d996e97e875459cccc838d
hym19940701 <412947075@qq.com>
2017-10-10 12:55:19 +0800

    制作banner部分

--- js/index.js
+++ js/index.js
@@ -3,23 +3,30 @@
  */
 function animat (obj,target,speed){
   target=target||0;
-  speed=speed||3
+  speed=speed||1
   var height
   clearInterval(obj.timer)
   obj.timer=setInterval(function(){
-     if(obj.offsetHeight<target) {
-        height = obj.offsetHeight + speed
+     if(target-obj.offsetHeight<speed) {
+       obj.style.height=target+'px'
+       clearInterval(obj.timer)
      }
-       obj.style.height=height+'px'
+     // console.log(obj.offsetHeight-target)
+    height = obj.offsetHeight + speed
+    obj.style.height=height+'px'
   },10)
-  return obj
 }
-var test=document.getElementsByClassName('dropdownmenus')[0];
+var dropdownmenus=document.getElementsByClassName('dropdownmenus')[0];
 var channel=document.getElementsByClassName('channel')[0];
-channel.onmouseover=function(){
-  animat(test,200,5)
-}
-channel.onmouseout=function(){
-  test.style.height=0
-  clearInterval(animat().timer)
-}
+channel.addEventListener('mouseover',function(){
+  animat(dropdownmenus,200,5)
+  dropdownmenus.onmouseover=function(){
+    dropdownmenus.onmouseout=function(){
+      console.log(dropdownmenus.children[0])
+        dropdownmenus.style.height=0
+        clearInterval(dropdownmenus.timer)
+    }
+  }
+},false)
+
+
